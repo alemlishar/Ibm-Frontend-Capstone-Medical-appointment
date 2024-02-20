a80405044e8e106483baa5dc6401168eadf660f0
alemlishar <alextopten03@gmail.com>
2024-02-20 12:28:47 -0500

    notification route'

--- src/App.js
+++ src/App.js
@@ -5,19 +5,20 @@ import Navbar from './Components/Navbar/Navbar';
 import Login from './Components/Login/Login';
 import Sign_Up from './Components/Sign_Up/Sign_Up';
 import InstantConsultation from './Components/InstantConsultationBooking/InstantConsultation'
+import Notification from './Components/Notification/Notification'
 
 function App() {
   return (
     <>
               <BrowserRouter>
-          <Navbar/>
+          <Notification>
               <Routes>
               <Route path="/" element={<Landing_Page/>}/>
               <Route path="/Login" element={<Login/>}/>
               <Route path="/Sign_Up" element={<Sign_Up/>}/>
               <Route path="/instant-consultation" element={<InstantConsultation />} />
               </Routes>
-            
+            </Notification>
         </BrowserRouter>
           </>
   );
