9d41c91137cb439b56d8590f09c765a673134ba0
alemlishar <alextopten03@gmail.com>
2024-02-20 10:51:37 -0500

    review form component implmentation

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:MTAyMy1hbGV4dG9w@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:MjYyMzktYWxleHRv@127.0.0.1:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
